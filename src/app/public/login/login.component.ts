9cfe2be628bb1311016628d73f92e31bf53844f8
Juan Aranda-Alvarez <jaranda@a3nstudio.com>
2023-07-12 23:51:57 -0500

    Getting data from the form.

--- src/app/public/login/login.component.ts
+++ src/app/public/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: [ '../public.component.scss', './login.component.scss',]
 })
 export class LoginComponent {
+  form!: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {
+  }
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      email: '',
+      password: ''
+    });
+  }
+
+  submit() {
+    console.log(this.form.getRawValue())
+  }
 }
